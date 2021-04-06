c632cdd550ba0654c10d2e4f843eb9f46facb0de
Andi Kleen <ak@linux.intel.com>
2021-04-06 08:18:02 -0700

    objtool: Fix segfault
    I had some segfaults with objtool running over gcc LTO generated
    vmlinux.o. In some cases the special instructions could end up
    with a zero section, which breaks code later that tries to print
    warning message for them.
    
    Fix the error handling to ignore the instruction if there is no
    section early.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/objtool/special.c
+++ tools/objtool/special.c
@@ -82,6 +82,7 @@ static int get_alt_entry(struct elf *elf, struct special_entry *entry,
 						   entry->orig_len);
 		alt->new_len = *(unsigned char *)(sec->data->d_buf + offset +
 						  entry->new_len);
+		alt->new_sec = alt->orig_sec;
 	}
 
 	if (entry->feature) {
@@ -108,6 +109,11 @@ static int get_alt_entry(struct elf *elf, struct special_entry *entry,
 				  sec, offset + entry->new);
 			return -1;
 		}
+		if (!new_reloc->sym->sec) {
+			WARN_FUNC("can't find new reloc symbol sec",
+				  sec, offset + entry->new);
+			return -1;
+		}
 
 		reloc_to_sec_off(new_reloc, &alt->new_sec, &alt->new_off);
 
@@ -127,6 +133,9 @@ static int get_alt_entry(struct elf *elf, struct special_entry *entry,
 		}
 		alt->key_addend = key_reloc->addend;
 	}
+
+	if (!alt->new_sec)
+		return -1;
 
 	return 0;
 }
