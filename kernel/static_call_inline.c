551c43d432ecdde23cc5f61c7ad6cd8d295437dd
marxin <mliska@suse.cz>
2022-05-18 13:24:24 +0200

    Rename sc_selftest in static_call_inline.c.
    Rename it so that it won't collide with the same variable
    defined in static_call.c.

--- kernel/static_call_inline.c
+++ kernel/static_call_inline.c
@@ -501,17 +501,17 @@ early_initcall(static_call_init);
 
 #ifdef CONFIG_STATIC_CALL_SELFTEST
 
-static int func_a(int x)
+int __visible sci_func_a(int x)
 {
 	return x+1;
 }
 
-static int func_b(int x)
+int __visible sci_func_b(int x)
 {
 	return x+2;
 }
 
-DEFINE_STATIC_CALL(sc_selftest, func_a);
+DEFINE_STATIC_CALL(sci_selftest, sci_func_a);
 
 static struct static_call_data {
       int (*func)(int);
@@ -519,8 +519,8 @@ static struct static_call_data {
       int expect;
 } static_call_data [] __initdata = {
       { NULL,   2, 3 },
-      { func_b, 2, 4 },
-      { func_a, 2, 3 }
+      { sci_func_b, 2, 4 },
+      { sci_func_a, 2, 3 }
 };
 
 static int __init test_static_call_init(void)
@@ -531,9 +531,9 @@ static int __init test_static_call_init(void)
 	      struct static_call_data *scd = &static_call_data[i];
 
               if (scd->func)
-                      static_call_update(sc_selftest, scd->func);
+                      static_call_update(sci_selftest, scd->func);
 
-              WARN_ON(static_call(sc_selftest)(scd->val) != scd->expect);
+              WARN_ON(static_call(sci_selftest)(scd->val) != scd->expect);
       }
 
       return 0;
